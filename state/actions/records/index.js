815179ce435fe87cc3e81edf4740568fa8fb40dd
Memet <meavci@gmail.com>
2021-08-08 21:42:19 -0500

    create action file

---
+++ state/actions/records/index.js
@@ -0,0 +1,31 @@
+import{mockFetch} from"../../../util/mockFetch/index";
+const actionTypes={
+  LOAD_RECORDS_ERROR:'records/loadRecordsError',
+  LOAD_RECORDS_REQUEST:'records/loadRecordsRequest',
+  LOAD_RECORDS_SUCCESS:'records/loadRecordsSuccess',
+
+}
+const loadRecordsRequest=(payload)=>{
+  return(dispatch)=>{
+    dispatch({
+      type:actionTypes.LOAD_RECORDS_REQUEST,
+    })
+    mockFetch()
+    .then((response)=>{
+      dispatch({
+        payload:response,
+        type:actionTypes.LOAD_RECORDS_SUCCESS
+      })
+    })
+    .catch((error)=>{
+      dispatch({
+        payload:error,
+        type:actionTypes.LOAD_RECORDS_ERROR
+      })
+    })
+  }
+}
+const actionCreators={
+  loadRecordsRequest,
+};
+export{actionCreators,actionTypes}
